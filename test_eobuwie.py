8216d7c63790782663567dc661cbf1dea5ae7296
Karol Kolanski <gralu86@gmail.com>
2022-10-16 13:27:54 +0200

    Dodano obsługę Chrome

--- test_eobuwie.py
+++ test_eobuwie.py
@@ -24,7 +24,7 @@ class RejestracjaNowegoUzytkownika(unittest.TestCase):
         # WARUNKI WSTĘPNE
         # 1. Otwarta strona główna
         # self.driver = webdriver.Chrome()
-        self.driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub', desired_capabilities=DesiredCapabilities.EDGE)
+        self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.get("https://www.eobuwie.com.pl/")
         # (2. Użytkownik niezalogowany)
